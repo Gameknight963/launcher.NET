f78547a972fb0de0a4ec9819c16db76366fc58b4
sebi <sebisebpaw@gmail.com>
2026-02-12 22:49:02 -0600

    some ui stuff (in progress)

--- LauncherForm.cs
+++ LauncherForm.cs
@@ -2,13 +2,17 @@ using System.Runtime.InteropServices.Marshalling;
 
 namespace launcherdotnet
 {
+    using Microsoft.VisualBasic;
     using System.IO;
     public partial class LauncherForm : System.Windows.Forms.Form
     {
+        public string IdleStatus = "Status will appear here";
+        public LauncherData Data = LauncherDataManager.ReadLauncherData();
         public LauncherForm()
         {
             InitializeComponent();
-            UpdateGameList(gamesView, LauncherDataManager.ReadLauncherData());
+            UpdateGameList(gamesView, Data);
+            SetStatus(IdleStatus);
         }
         public void SetStatus(string text)
         {
@@ -19,30 +23,61 @@ namespace launcherdotnet
             gamesView.Items.Clear();
             foreach (var game in data.Versions)
             {
-                gamesView.Items.Add(game.Label);
+                ListViewItem item = new ListViewItem(game.Label);
+                item.SubItems.Add(game.Path);
+                gamesView.Items.Add(item);
             }
         }
-        //public void UpdateGameList(ListBox listBox, LauncherData data)
-        //{
-        //    listBox.Items.Clear();
-        //    foreach (var game in data.Versions)
-        //    {
-        //        listBox.Items.Add(game.Label);
-        //    }
-        //}
-        private void gamesList_SelectedIndexChanged(object sender, EventArgs e)
-        {
 
+        public void AddNewInstance(string label)
+        {
+            RereadLauncherData();
+            LauncherData data = Data;
+            data.Versions.Add(new GameInfo
+            {
+                Label = label,
+            });
+            Data = data;
+            LauncherDataManager.SaveLauncherData(data);
+        }
+        public void RereadLauncherData()
+        {
+            Data = LauncherDataManager.ReadLauncherData();
         }
 
         private async void button1_Click(object sender, EventArgs e)
         {
             await Install.DownloadAndInstallGameAsync(
-                "bujehvbe", 
+                "bujehvbe",
                 Path.Combine(Directory.GetCurrentDirectory(), "Games"),
                 "salamalonekabatrabaslatrowerebakaedro",
                 SetStatus);
-            UpdateGameList(gamesView, LauncherDataManager.ReadLauncherData());
+            RereadLauncherData(); // MUST do this here
+            UpdateGameList(gamesView, Data);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string result = Interaction.InputBox(
+                "Enter a label for this instance:",
+                "Set Game Label");
+            AddNewInstance(result);
+            UpdateGameList(gamesView, Data);
+        }
+
+        private void gamesView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (gamesView.SelectedItems.Count < 1)
+            {
+                SetStatus(IdleStatus);
+                return;
+            }
+            ListViewItem selectedItem = gamesView.SelectedItems[0];
+            SetStatus($"Selected: {selectedItem.Text}");
+            if (selectedItem.SubItems[1].Text == "")
+            {
+                Console.WriteLine("eujfwufhn");
+            }
         }
     }
 }
