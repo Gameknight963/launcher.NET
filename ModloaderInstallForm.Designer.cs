4b5612b8b7c5de96e2adbb6aaecb30c25ed7c308
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-02-15 04:41:49 -0600

    wip melonloader installation

---
+++ ModloaderInstallForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace launcherdotnet
+{
+    partial class ModloaderInstallForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ModloaderDropdown = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            VersionDropdown = new ComboBox();
+            InstallModloaderButton = new Button();
+            progressBar = new ProgressBar();
+            SuspendLayout();
+            // 
+            // ModloaderDropdown
+            // 
+            ModloaderDropdown.FormattingEnabled = true;
+            ModloaderDropdown.Items.AddRange(new object[] { "Melonloader" });
+            ModloaderDropdown.Location = new Point(41, 30);
+            ModloaderDropdown.Name = "ModloaderDropdown";
+            ModloaderDropdown.Size = new Size(121, 23);
+            ModloaderDropdown.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(41, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(168, 15);
+            label1.TabIndex = 1;
+            label1.Text = "What would you like to install?";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(41, 66);
+            label2.Name = "label2";
+            label2.Size = new Size(215, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Which version would you like to install?";
+            // 
+            // VersionDropdown
+            // 
+            VersionDropdown.FormattingEnabled = true;
+            VersionDropdown.Location = new Point(41, 84);
+            VersionDropdown.Name = "VersionDropdown";
+            VersionDropdown.Size = new Size(121, 23);
+            VersionDropdown.TabIndex = 0;
+            // 
+            // InstallModloaderButton
+            // 
+            InstallModloaderButton.Location = new Point(41, 127);
+            InstallModloaderButton.Name = "InstallModloaderButton";
+            InstallModloaderButton.Size = new Size(75, 23);
+            InstallModloaderButton.TabIndex = 2;
+            InstallModloaderButton.Text = "Install";
+            InstallModloaderButton.UseVisualStyleBackColor = true;
+            InstallModloaderButton.Click += InstallModloader_Click;
+            // 
+            // progressBar
+            // 
+            progressBar.Location = new Point(41, 161);
+            progressBar.Name = "progressBar";
+            progressBar.Size = new Size(430, 23);
+            progressBar.TabIndex = 4;
+            // 
+            // ModloaderInstallForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(517, 196);
+            Controls.Add(progressBar);
+            Controls.Add(InstallModloaderButton);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(VersionDropdown);
+            Controls.Add(ModloaderDropdown);
+            Name = "ModloaderInstallForm";
+            Text = "Installation";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox ModloaderDropdown;
+        private Label label1;
+        private Label label2;
+        private ComboBox VersionDropdown;
+        private Button InstallModloaderButton;
+        private ProgressBar progressBar;
+    }
+}
---
+++ ModloaderInstallForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Threading.Tasks;
+using MLInstallerSDK;
+using Microsoft.VisualBasic;
+
+namespace launcherdotnet
+{
+    public partial class ModloaderInstallForm : Form
+    {
+        List<MLVersion> versions = new List<MLVersion>();
+        GameInfo game = new GameInfo();
+
+        public ModloaderInstallForm(GameInfo userChosenGame)
+        {
+            InitializeComponent();
+            game = userChosenGame;
+            this.Icon = SystemIcons.Information;
+            ModloaderDropdown.SelectedIndex = 0;
+            Initialize();
+        }
+
+        public async void Initialize()
+        {
+            InstallModloaderButton.Enabled = false;
+            versions = await MLInstallerSDK.MLManager.FetchAvailableVersionsAsync();
+            foreach (MLVersion v in versions)
+            {
+                VersionDropdown.Items.Add(v.Version);
+            }
+            if (VersionDropdown.Items.Count > 0)
+                VersionDropdown.SelectedIndex = 0;
+            InstallModloaderButton.Enabled = true;
+        }
+
+        private async void InstallModloader_Click(object sender, EventArgs e)
+        {
+            if (ModloaderDropdown.SelectedItem == null)
+            {
+                MessageBox.Show("You must select something to be installed.",
+                    "Invalid selection",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+                return;
+            }
+                if (VersionDropdown.SelectedItem == null)
+            {
+                MessageBox.Show("You must select a version.",
+                    "Invalid selection",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+                return;
+            }
+            string? path = Path.GetDirectoryName(game.Path);
+            if (path == null)
+            {
+                MessageBox.Show("Game does not have a path!",
+                    "Invalid operation",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return;
+            }
+            MessageBox.Show("Installing...",
+                "Notice",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+            var progress = new Progress<double>(percent =>
+            {
+                if (progressBar.Style != ProgressBarStyle.Continuous)
+                    progressBar.Style = ProgressBarStyle.Continuous;
+                progressBar.Value = (int)percent;
+            });
+            Installer.Log = msg => Console.WriteLine(msg);
+            progressBar.Style = ProgressBarStyle.Marquee;
+            progressBar.MarqueeAnimationSpeed = 30;
+            bool success = await Installer.DownloadVersionAsync(versions[VersionDropdown.SelectedIndex], Path.Combine(path, "installed.zip"), Installer.Architecture.x64, progress);
+            progressBar.Style = ProgressBarStyle.Continuous;
+            progressBar.Value = 100;
+            if (success == true)
+            {
+                MessageBox.Show($"Installation complete", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show($"Installer failed", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
