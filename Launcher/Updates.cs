373c7f3b5be3f04af7205af9c268e4a9598849f0
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-02-16 15:05:37 -0600

    update README.md

---
+++ Launcher/Updates.cs
@@ -0,0 +1,16 @@
+﻿using Semver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace launcherdotnet.Launcher
+{
+    internal class Updater
+    {
+        private string _repo = "https://github.com/Gameknight963/launcher.NET";
+        public static async Task<SemVersion?> GetLatestVersionAsync()
+        {
+
+        }
+    }
+}
