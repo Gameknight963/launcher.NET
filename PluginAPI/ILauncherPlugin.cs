40763771820068678636b83d8353944ce655a1ae
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-02-20 22:18:09 -0600

    add game installer game names, plugin descriptions

--- PluginAPI/ILauncherPlugin.cs
+++ PluginAPI/ILauncherPlugin.cs
@@ -7,7 +7,22 @@ namespace launcherdotnet.PluginAPI
 {
     public interface ILauncherPlugin
     {
+        /// <summary>
+        /// The name of your plugin.
+        /// </summary>
         string Name { get; }
+
+        /// <summary>
+        /// Description of your plugin to show
+        /// in UI. Optional, set to null if you don't
+        /// want it, but highly recommended.
+        /// </summary>
+        string Description { get; }
+
+        /// <summary>
+        /// The version of the launcher.NET
+        /// plugin API your plugin was built for.
+        /// </summary>
         SemVersion TargetApiVersion { get; }
 
         /// <summary>
