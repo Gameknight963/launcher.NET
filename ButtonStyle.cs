e65d52a7c317d5a7e877c15257adeb9f1ebc6cf3
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-03-29 16:46:33 -0700

    fix button borders in three styles

---
+++ ButtonStyle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace launcherdotnet
+{
+    public class ButtonStyle : ControlStyle
+    {
+        public FlatStyle? FlatStyle { get; set; }
+        public int? BorderSize { get; set; }
+        public Color? BorderColor { get; set; }
+        public Color? HoverBackColor { get; set; }
+        public Color? DownBackColor { get; set; }
+
+        public ButtonStyle(
+            Color? backColor = null,
+            Color? foreColor = null,
+            FlatStyle? flatStyle = null,
+            int? borderSize = null,
+            Color? borderColor = null,
+            Color? hoverBackColor = null,
+            Color? downBackColor = null,
+            Font? font = null)
+        {
+            BackColor = backColor;
+            ForeColor = foreColor;
+            FlatStyle = flatStyle;
+            BorderSize = borderSize;
+            BorderColor = borderColor;
+            HoverBackColor = hoverBackColor;
+            DownBackColor = downBackColor;
+            Font = font;
+        }
+    }
+}
--- ControlStyle.cs
+++ ControlStyle.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace launcherdotnet.Launcher
+namespace launcherdotnet
 {
     public class ControlStyle
     {
