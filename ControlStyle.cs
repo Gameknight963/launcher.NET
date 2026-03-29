f923ea343689784405dedb8dbbd63ec224534eab
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-03-29 03:23:15 -0700

    wip theme system

---
+++ ControlStyle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace launcherdotnet.Launcher
+{
+    public class ControlStyle
+    {
+        public Color? BackColor { get; set; }
+        public Color? ForeColor { get; set; }
+        public BorderStyle? BorderStyle { get; set; }
+        public Font? Font { get; set; }
+        
+        public ControlStyle(
+            Color? backColor = null,
+            Color? foreColor = null,
+            BorderStyle? borderStyle = null,
+            Font? font = null)
+        {
+            BackColor = backColor;
+            ForeColor = foreColor;
+            BorderStyle = borderStyle;
+            Font = font;
+        }
+    }
+}
