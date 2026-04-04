f917e926015793939263e879b16fa01bbafecf08
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-04-03 22:13:43 -0500

    optimize ThemeableForm

--- Syling/ThemeableForm.cs
+++ Syling/ThemeableForm.cs
@@ -11,6 +11,8 @@ namespace launcherdotnet.Syling
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public ThemeManager.Theme ActiveTheme { get; set; }
 
+        private readonly HashSet<Control> _themedControls = new();
+
         public enum TextRenderMode
         {
             Auto,
@@ -29,49 +31,80 @@ namespace launcherdotnet.Syling
         {
             if (c is ListView lv)
             {
+                if (_themedControls.Add(lv))
+                {
+                    lv.DrawColumnHeader += Lv_DrawColumnHeader;
+                    lv.DrawItem += Lv_DrawItem;
+                    lv.DrawSubItem += Lv_DrawSubItem;
+                }
+
                 lv.OwnerDraw = (theme != ThemeManager.Theme.Light);
-
-                lv.DrawColumnHeader -= Lv_DrawColumnHeader;
-                lv.DrawColumnHeader += Lv_DrawColumnHeader;
-
-                lv.DrawItem -= Lv_DrawItem;
-                lv.DrawItem += Lv_DrawItem;
-
-                lv.DrawSubItem -= Lv_DrawSubItem;
-                lv.DrawSubItem += Lv_DrawSubItem;
             }
-             
+
             if (c is TabControl tc)
             {
+                if (_themedControls.Add(tc))
+                {
+                    tc.DrawItem += Tc_DrawItem;
+                }
+
                 if (theme == ThemeManager.Theme.Light)
                 {
                     tc.DrawMode = TabDrawMode.Normal;
                     return;
                 }
-                tc.DrawMode = TabDrawMode.OwnerDrawFixed;
 
-                tc.DrawItem -= Tc_DrawItem;
-                tc.DrawItem += Tc_DrawItem;
+                tc.DrawMode = TabDrawMode.OwnerDrawFixed;
             }
 
             foreach (Control child in c.Controls)
                 ApplyControlTheme(child, theme);
         }
 
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                foreach (Control c in _themedControls)
+                {
+                    if (c is ListView lv)
+                    {
+                        lv.DrawColumnHeader -= Lv_DrawColumnHeader;
+                        lv.DrawItem -= Lv_DrawItem;
+                        lv.DrawSubItem -= Lv_DrawSubItem;
+                    }
+                    else if (c is TabControl tc)
+                    {
+                        tc.DrawItem -= Tc_DrawItem;
+                    }
+                }
+
+                _themedControls.Clear();
+            }
+
+            base.Dispose(disposing);
+        }
+
         public void SetTextRenderMode(TextRenderMode mode)
         {
-            if (mode == TextRenderMode.Auto)
-                _useGdiText = !(ActiveTheme == ThemeManager.Theme.Acrylic && ActiveTheme == ThemeManager.Theme.Blur);
+            switch (mode)
+            {
+                case TextRenderMode.Auto:
+                    _useGdiText = !(ActiveTheme == ThemeManager.Theme.Acrylic || ActiveTheme == ThemeManager.Theme.Blur);
+                    break;
 
-            if (mode == TextRenderMode.AutoStrict)
-                _useGdiText = (ActiveTheme == ThemeManager.Theme.Light && ActiveTheme == ThemeManager.Theme.Dark);
+                case TextRenderMode.AutoStrict:
+                    _useGdiText = ActiveTheme == ThemeManager.Theme.Light || ActiveTheme == ThemeManager.Theme.Dark;
+                    break;
 
-            if (mode == TextRenderMode.Gdi)
-                _useGdiText = true;
+                case TextRenderMode.Gdi:
+                    _useGdiText = true;
+                    break;
 
-            if (mode == TextRenderMode.DrawString)
-                _useGdiText = false;
-
+                case TextRenderMode.DrawString:
+                    _useGdiText = false;
+                    break;
+            }
         }
 
         public void ApplyTheme(ThemeManager.Theme theme, TextRenderMode? textMode = null)
@@ -80,103 +113,106 @@ namespace launcherdotnet.Syling
             {
                 case ThemeManager.Theme.Light:
                     break;
+
                 case ThemeManager.Theme.Dark:
                     _headerStyle.ForeColor = Color.White;
                     _headerStyle.BackColor = ThemeManager.DarkMainColor;
                     break;
+
                 case ThemeManager.Theme.ExtendFrame:
                 case ThemeManager.Theme.ExtendFrameDark:
                     _headerStyle.ForeColor = Color.White;
                     _headerStyle.BackColor = Color.Black;
                     break;
+
                 case ThemeManager.Theme.Blur:
                 case ThemeManager.Theme.Acrylic:
                     _headerStyle.ForeColor = Color.White;
                     _headerStyle.BackColor = ThemeManager.AcrylicMainColor;
                     break;
             }
+
             ActiveTheme = theme;
+
             ApplyControlTheme(this, theme);
             ThemeManager.ApplyThemeToForm(this, theme);
-            if (textMode.HasValue) SetTextRenderMode(textMode.Value);
+
+            if (textMode.HasValue)
+                SetTextRenderMode(textMode.Value);
+        }
+
+        private void DrawShadowText(Graphics g, string text, Font font, Rectangle bounds, Color textColor)
+        {
+            Color shadowColor = Color.FromArgb(120, 0, 0, 0);
+
+            TextRenderer.DrawText(
+                g,
+                text,
+                font,
+                new Rectangle(bounds.X + 1, bounds.Y + 1, bounds.Width, bounds.Height),
+                shadowColor,
+                TextFormatFlags.Left
+            );
+
+            TextRenderer.DrawText(
+                g,
+                text,
+                font,
+                bounds,
+                textColor,
+                TextFormatFlags.Left
+            );
         }
 
         private void Lv_DrawSubItem(object? sender, DrawListViewSubItemEventArgs e)
         {
+            Color back = e.Item!.Selected ? SystemColors.Highlight : e.Item.BackColor;
+
+            using (Brush backBrush = new SolidBrush(back))
+            {
+                e.Graphics.FillRectangle(backBrush, e.Bounds);
+            }
+
             if (_useGdiText)
             {
-                using Brush backBrush = new SolidBrush(e.Item!.Selected ? SystemColors.Highlight : e.Item.BackColor);
                 using Brush foreBrush = new SolidBrush(_headerStyle.ForeColor!.Value);
-                e.Graphics.FillRectangle(backBrush, e.Bounds);
                 e.Graphics.DrawString(e.SubItem!.Text, e.SubItem.Font, foreBrush, e.Bounds);
             }
             else
             {
-                using Brush backBrush = new SolidBrush(e.Item!.Selected ? SystemColors.Highlight : e.Item.BackColor);
-                e.Graphics.FillRectangle(backBrush, e.Bounds);
-
-                string text = e.SubItem!.Text;
-                Font font = e.SubItem.Font;
-                Rectangle bounds = e.Bounds;
-
-                Color textColor = _headerStyle.ForeColor!.Value;
-                Color shadowColor = Color.FromArgb(120, 0, 0, 0);
-
-                TextRenderer.DrawText(
+                DrawShadowText(
                     e.Graphics,
-                    text,
-                    font,
-                    new Rectangle(bounds.X + 1, bounds.Y + 1, bounds.Width, bounds.Height),
-                    shadowColor,
-                    TextFormatFlags.Left
-                );
-
-                TextRenderer.DrawText(
-                    e.Graphics,
-                    text,
-                    font,
-                    bounds,
-                    textColor,
-                    TextFormatFlags.Left
+                    e.SubItem!.Text,
+                    e.SubItem.Font,
+                    e.Bounds,
+                    _headerStyle.ForeColor!.Value
                 );
             }
         }
 
         private void Lv_DrawColumnHeader(object? sender, DrawListViewColumnHeaderEventArgs e)
         {
+            using (SolidBrush backBrush = new(_headerStyle.BackColor!.Value))
+            {
+                e.Graphics.FillRectangle(backBrush, e.Bounds);
+            }
+
             if (_useGdiText)
             {
-                using SolidBrush backBrush = new(_headerStyle.BackColor!.Value);
-                using SolidBrush foreBrush = new(_headerStyle.ForeColor!.Value);
-
-                e.Graphics.FillRectangle(backBrush, e.Bounds);
+                using Brush foreBrush = new SolidBrush(_headerStyle.ForeColor!.Value);
                 e.Graphics.DrawString(e.Header!.Text, e.Font!, foreBrush, e.Bounds);
             }
             else
             {
-                using SolidBrush backBrush = new(_headerStyle.BackColor!.Value);
-                e.Graphics.FillRectangle(backBrush, e.Bounds);
-
-                TextRenderer.DrawText(
-                    e.Graphics,
-                    e.Header!.Text,
-                    e.Font!,
-                    new Rectangle(e.Bounds.X + 1, e.Bounds.Y + 1, e.Bounds.Width, e.Bounds.Height),
-                    Color.FromArgb(120, 0, 0, 0),
-                    TextFormatFlags.Left
-                );
-
-                TextRenderer.DrawText(
+                DrawShadowText(
                     e.Graphics,
                     e.Header!.Text,
                     e.Font!,
                     e.Bounds,
-                    _headerStyle.ForeColor!.Value,
-                    TextFormatFlags.Left
+                    _headerStyle.ForeColor!.Value
                 );
             }
         }
-
 
         private void Lv_DrawItem(object? sender, DrawListViewItemEventArgs e) => e.DrawDefault = false;
 
@@ -187,7 +223,7 @@ namespace launcherdotnet.Syling
 
             Rectangle bounds = tc.GetTabRect(e.Index);
 
-            using SolidBrush backBrush = new(_headerStyle.BackColor!.Value);
+            using (SolidBrush backBrush = new(_headerStyle.BackColor!.Value))
             {
                 e.Graphics.FillRectangle(backBrush, bounds);
 
