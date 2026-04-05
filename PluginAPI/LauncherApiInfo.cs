d45e2fe29cf7bcffde7fd5e570344503a38ea332
sebi <sebisebpaw@gmail.com>
2026-04-05 16:57:36 -0500

    change api version from 0.6 to 0.7

--- PluginAPI/LauncherApiInfo.cs
+++ PluginAPI/LauncherApiInfo.cs
@@ -4,7 +4,7 @@ namespace launcherdotnet.PluginAPI
 {
     public class LauncherApiInfo
     {
-        public static readonly SemVersion ApiVersion = new(0, 6, 0);
+        public static readonly SemVersion ApiVersion = new(0, 7, 0);
         public static string ApiVersionString => ApiVersion.ToString();
     }
 }
