35c7e92632d6c86ca342e8ea0d167af14a2e7ea3
sebi <sebisebpaw@gmail.com>
2026-04-05 15:26:24 -0500

    add CoolInputBox

---
+++ Launcher/Forms/CoolInputBox.cs
@@ -0,0 +1,50 @@
+﻿using launcherdotnet.Helpers;
+using launcherdotnet.Syling;
+
+namespace launcherdotnet.Launcher.Forms
+{
+    public partial class CoolInputBox : ThemeableForm
+    {
+        public string? ResultText { get; private set; }
+
+        public static string? Prompt(string prompt,
+            string caption = "",
+            string defaultResponse = "",
+            string placeholder = "")
+        {
+            using CoolInputBox box = new(prompt, caption, defaultResponse, placeholder);
+            box.ShowDialog();
+            return box.ResultText;
+        }
+
+        public CoolInputBox(string prompt,
+            string caption = "",
+            string defaultResponse = "",
+            string placeholder = "")
+        {
+            InitializeComponent();
+            this.AcceptButton = okButton;
+            this.CancelButton = cancelButton;
+            okButton.DialogResult = DialogResult.OK;
+            cancelButton.DialogResult = DialogResult.Cancel;
+
+            label.Text = prompt;
+            this.Text = caption;
+            textBox.Text = defaultResponse;
+            TextBoxHelpers.SetPlaceholder(textBox, placeholder);
+            this.StartPosition = FormStartPosition.CenterParent;
+
+            textBox.Select();
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            ResultText = textBox.Text;
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            ResultText = null;
+        }
+    }
+}
