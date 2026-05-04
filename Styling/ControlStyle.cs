bb1d59fc7ea04808abbd15dc08fbf59e13ffa366
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-05-03 19:24:43 -0500

    fix styling typo (originally called syling)

---
+++ Styling/ControlStyle.cs
@@ -0,0 +1,22 @@
+﻿namespace launcherdotnet.Styling
+{
+    public class ControlStyle
+    {
+        public Color? BackColor { get; set; }
+        public Color? ForeColor { get; set; }
+        public BorderStyle? BorderStyle { get; set; }
+        public Font? Font { get; set; }
+        
+        public ControlStyle(
+            Color? backColor = null,
+            Color? foreColor = null,
+            BorderStyle? borderStyle = null,
+            Font? font = null)
+        {
+            BackColor = backColor;
+            ForeColor = foreColor;
+            BorderStyle = borderStyle;
+            Font = font;
+        }
+    }
+}
