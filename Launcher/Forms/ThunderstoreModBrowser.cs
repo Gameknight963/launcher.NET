94c003519f78cbb53505ef8afec95b1165f3a791
sebi <sebisebpaw@gmail.com>
2026-05-04 20:19:54 -0500

    listview virtualization, halfs ram usage on lethal company slug

--- Launcher/Forms/ThunderstoreModBrowser.cs
+++ Launcher/Forms/ThunderstoreModBrowser.cs
@@ -6,24 +6,30 @@ namespace launcherdotnet.Launcher.Forms
 {
     public partial class ThunderstoreModBrowser : ThemeableForm
     {
+        private List<ThunderstorePackage> _packages = [];
+
         public ThunderstoreModBrowser(GameInfo game)
         {
             InitializeComponent();
             CancelButton = cancelButton;
             AcceptButton = okButton;
+            modsLv.VirtualMode = true;
+            modsLv.RetrieveVirtualItem += ModsLv_RetrieveVirtualItem;
             UpdateModsLv(game);
+        }
+
+        private void ModsLv_RetrieveVirtualItem(object? sender, RetrieveVirtualItemEventArgs e)
+        {
+            e.Item = new ListViewItem(_packages[e.ItemIndex].Name);
         }
 
         async void UpdateModsLv(GameInfo game)
         {
             if (game.ThunderstoreCommunitySlug is not string slug) return;
             UseWaitCursor = true;
-            List<ThunderstorePackage> packages = await ThunderstoreClient.GetPackagesAsync(slug);
+            _packages = await ThunderstoreClient.GetPackagesAsync(slug);
+            modsLv.VirtualListSize = _packages.Count;
             UseWaitCursor = false;
-            foreach (ThunderstorePackage p in packages)
-            {
-                modsLv.Items.Add(p.Name);
-            }
         }
     }
 }
