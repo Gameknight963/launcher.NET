5664e2e768e330af690e5bca75b5371c54cbcd12
sebi <sebisebpaw@gmail.com>
2026-05-06 16:17:19 -0500

    image click handler (not doing anything yet), open all links in system browser

---
+++ Thunderstore/BrowserScriptHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace launcherdotnet.Thunderstore
+{
+    [System.Runtime.InteropServices.ComVisible(true)]
+    public class BrowserScriptHelper
+    {
+        public void OnImageClicked(string url)
+        {
+            LauncherLogger.WriteLine($"Image clicked: {url}");
+        }
+    }
+}
