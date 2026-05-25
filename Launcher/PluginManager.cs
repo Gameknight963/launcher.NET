235d29fdc520caf85b7aa350d8256cb86c885bde
sebi <sebisebpaw@gmail.com>
2026-05-25 14:20:55 -0500

    move using statements to top level

--- Launcher/PluginManager.cs
+++ Launcher/PluginManager.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Reflection;
+using launcherdotnet.PluginAPI;
 
 namespace launcherdotnet.Launcher
 {
-    using System.Reflection;
-    using launcherdotnet.PluginAPI;
 
     internal static class PluginManager
     {
