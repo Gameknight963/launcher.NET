7cb496fb5cee1d2584addc5d0fd7b52600096451
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-05-25 02:27:22 -0500

    write installed mods to launcehrdotnet-manifest

---
+++ Launcher/InstalledMod.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace launcherdotnet.Launcher
+{
+    public class InstalledMod
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; } = "";
+        [JsonProperty("owner")]
+        public string Owner { get; set; } = "";
+        [JsonProperty("version")]
+        public string Version { get; set; } = "";
+        [JsonProperty("files")]
+        public List<string> Files { get; set; } = [];
+        [JsonProperty("isDependency")]
+        public bool IsDependency { get; set; }
+        [JsonProperty("installedAt")]
+        public DateTime InstalledAt { get; set; } = DateTime.UtcNow;
+    }
+}
