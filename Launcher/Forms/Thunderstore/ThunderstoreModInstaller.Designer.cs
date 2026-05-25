4e58f993d7fb4363c6ba3b5fd9ff4addf594b499
sebi <sebisebpaw@gmail.com>
2026-05-24 21:25:50 -0500

    add second progress bar to keep track of individual mod download progress

--- Launcher/Forms/Thunderstore/ThunderstoreModInstaller.Designer.cs
+++ Launcher/Forms/Thunderstore/ThunderstoreModInstaller.Designer.cs
@@ -31,23 +31,24 @@
             activityHint = new Label();
             progressBar = new ProgressBar();
             logBox = new TextBox();
+            downloadProgressBar = new ProgressBar();
             SuspendLayout();
             // 
             // activityHint
             // 
             activityHint.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             activityHint.AutoSize = true;
-            activityHint.Location = new Point(41, 155);
+            activityHint.Location = new Point(41, 190);
             activityHint.Name = "activityHint";
             activityHint.Size = new Size(167, 15);
             activityHint.TabIndex = 7;
             activityHint.Text = "Installation hint will show here";
-            activityHint.Visible = false;
+            activityHint.Visible = true;
             // 
             // progressBar
             // 
             progressBar.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            progressBar.Location = new Point(41, 129);
+            progressBar.Location = new Point(41, 135);
             progressBar.Name = "progressBar";
             progressBar.Size = new Size(439, 23);
             progressBar.TabIndex = 8;
@@ -59,17 +60,27 @@
             logBox.Multiline = true;
             logBox.Name = "logBox";
             logBox.ReadOnly = true;
-            logBox.Size = new Size(439, 111);
+            logBox.Size = new Size(439, 117);
             logBox.TabIndex = 10;
+            // 
+            // downloadProgressBar
+            // 
+            downloadProgressBar.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            downloadProgressBar.Location = new Point(41, 164);
+            downloadProgressBar.Name = "downloadProgressBar";
+            downloadProgressBar.Size = new Size(439, 23);
+            downloadProgressBar.TabIndex = 8;
             // 
             // ThunderstoreModInstaller
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(517, 182);
+            ClientSize = new Size(517, 217);
             Controls.Add(logBox);
+            Controls.Add(downloadProgressBar);
             Controls.Add(progressBar);
             Controls.Add(activityHint);
+            ForeColor = Color.Black;
             Name = "ThunderstoreModInstaller";
             Text = "Mod installation";
             ResumeLayout(false);
@@ -80,5 +91,6 @@
         private Label activityHint;
         private ProgressBar progressBar;
         private TextBox logBox;
+        private ProgressBar downloadProgressBar;
     }
 }
