e5c3ca1cce5f21577818991e973bb27ed8af6a5b
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-06-02 21:54:33 -0500

    add GameInstallerBase abstract class

---
+++ PluginAPI/GameInstallerBase.cs
@@ -0,0 +1,25 @@
+﻿namespace launcherdotnet.PluginAPI
+{
+    public abstract class GameInstallerBase : IGameInstaller
+    {
+        /// <inheritdoc />
+        public virtual Task Initialize() => Task.CompletedTask;
+
+        /// <inheritdoc />
+        public virtual IEnumerable<string>? GetReleases() => null;
+
+        /// <inheritdoc />
+        public virtual LabelQueryTime PromptForLabel =>
+            LabelQueryTime.Never;
+
+        /// <inheritdoc/>
+        public abstract string GameName { get; }
+
+        /// <inheritdoc />
+        public abstract Task<PluginGameInfo?> Install(
+            string installDir,
+            IProgress<double> progress,
+            IProgress<string> status,
+            string? version = null);
+    }
+}
