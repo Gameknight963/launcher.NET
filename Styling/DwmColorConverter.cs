4b2e90323bc0069958c7d0595157be4fdc5f90d0
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-06-02 23:34:11 -0500

    make DwmColor converter

---
+++ Styling/DwmColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace launcherdotnet.Styling
+{
+    public class DwmColorConverter : JsonConverter<DwmColor>
+    {
+        public override void WriteJson(JsonWriter writer, DwmColor? value, JsonSerializer serializer)
+        {
+            if (value == null)
+            {
+                writer.WriteNull();
+                return;
+            }
+
+            writer.WriteValue(value.ToAbgr());
+        }
+
+        public override DwmColor ReadJson(JsonReader reader, Type objectType, DwmColor? existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            if (reader.Value == null)
+                return new DwmColor(Color.Empty);
+
+            int abgr = Convert.ToInt32(reader.Value);
+            return DwmColor.FromAbgr(abgr);
+        }
+    }
+}
