025779997de4cb37a5d14405698f2fb6b4536665
sebi <sebisebpaw@gmail.com>
2026-06-04 22:05:17 -0500

    add CheckerboardPanel control

---
+++ Launcher/Controls/CheckerboardPanel.cs
@@ -0,0 +1,33 @@
+﻿namespace launcherdotnet.Launcher.Controls
+{
+    public class CheckerboardPanel : Panel
+    {
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            int cellSize = 8;
+            using SolidBrush lightBrush = new SolidBrush(Color.White);
+            using SolidBrush darkBrush = new SolidBrush(Color.LightGray);
+            for (int y = 0; y < ClientRectangle.Height; y += cellSize)
+            {
+                for (int x = 0; x < ClientRectangle.Width; x += cellSize)
+                {
+                    bool isLight = ((x / cellSize) + (y / cellSize)) % 2 == 0;
+
+                    e.Graphics.FillRectangle(
+                        isLight ? lightBrush : darkBrush,
+                        ClientRectangle.X + x,
+                        ClientRectangle.Y + y,
+                        cellSize,
+                        cellSize);
+                }
+            }
+            using SolidBrush brush = new SolidBrush(BackColor);
+            e.Graphics.FillRectangle(brush, ClientRectangle);
+        }
+        public CheckerboardPanel()
+        {
+            DoubleBuffered = true;
+            ResizeRedraw = true;
+        }
+    }
+}
