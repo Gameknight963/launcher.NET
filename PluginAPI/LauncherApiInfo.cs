dc9430299c121a629841d51fb4cfe6a7d2185cdb
Gameknight963 <sebisebpaw@gmail.com>
2026-06-29 21:58:53 -0500

    bump PluginAPI version, 2.0.0 => 2.0.1

--- PluginAPI/LauncherApiInfo.cs
+++ PluginAPI/LauncherApiInfo.cs
@@ -4,7 +4,7 @@ namespace launcherdotnet.PluginAPI
 {
     public class LauncherApiInfo
     {
-        public static readonly SemVersion ApiVersion = new(2, 0, 0);
+        public static readonly SemVersion ApiVersion = new(2, 0, 1);
         public static string ApiVersionString => ApiVersion.ToString();
     }
 }
