8c2425951efa4838d516e8b8f91b964e985e6d3d
gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-08-05 10:05:04 +0800

    add overloads for loading with datadirectory

--- PluginAPI/ModSourceConfig.cs
+++ PluginAPI/ModSourceConfig.cs
@@ -5,9 +5,9 @@ namespace launcherdotnet.PluginAPI
 {
     public abstract class ModSourceConfig<T> where T : ModSourceConfig<T>, new()
     {
-        public static T Load(GameInfo game, string sourceId)
+        public static T Load(string gameDataDirectory, string sourceId)
         {
-            string path = Path.Combine(game.DataDirectory, $"{sourceId}.json");
+            string path = Path.Combine(gameDataDirectory, $"{sourceId}.json");
             if (!File.Exists(path)) return new T();
             T instance = File.Exists(path)
                 ? JsonConvert.DeserializeObject<T>(File.ReadAllText(path)) ?? new T()
@@ -15,11 +15,15 @@ namespace launcherdotnet.PluginAPI
             return instance;
         }
 
-        public void Save(GameInfo game, string sourceId)
+        public static T Load(GameInfo game, string sourceId) => Load(game.DataDirectory, sourceId);
+
+        public void Save(string gameDataDirectory, string sourceId)
         {
-            string path = Path.Combine(game.DataDirectory, $"{sourceId}.json");
+            string path = Path.Combine(gameDataDirectory, $"{sourceId}.json");
             Directory.CreateDirectory(Path.GetDirectoryName(path)!);
             File.WriteAllText(path, JsonConvert.SerializeObject(this, Formatting.Indented));
         }
+
+        public void Save(GameInfo game, string sourceId) => Save(game.DataDirectory, sourceId);
     }
 }
