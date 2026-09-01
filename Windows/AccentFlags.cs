1d9c0ba681fd59ad5d78dafb482d7291c7e8d76b
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-08-31 20:13:45 -0500

    make enum value sharing intent clearer

--- Windows/AccentFlags.cs
+++ Windows/AccentFlags.cs
@@ -14,24 +14,18 @@ namespace launcherdotnet.Windows
         ACCENT_FLAG_NONE = 0,
 
         /// <summary>
+        /// Enables the gradient color (without this flag, the OS chooses).
+        /// </summary>
+        ACCENT_FLAG_ENABLE_GRADIENT_COLOR = 1 << 1,
+
+        /// <summary>
         /// Same bit as <see cref="ACCENT_FLAG_ENABLE_GRADIENT_COLOR" />,
         /// but results in the modern acrylic recipe under these conditions:
         /// <br/>
         /// 1. The operating system is Windows 11 22H2+ <br/>
-        /// 2. <see cref="launcherdotnet.Windows.AccentState.ACCENT_ENABLE_ACRYLICBLURBEHIND" /> is enabled.
+        /// 2. <see cref="AccentState.ACCENT_ENABLE_ACRYLICBLURBEHIND" /> is enabled.
         /// </summary>
-        ACCENT_FLAG_ENABLE_MODERN_ACRYLIC_RECIPE = 1 << 1,
-
-        /// <summary>
-        /// Enables the gradient color (without this flag, the OS chooses).
-        /// </summary>
-#pragma warning disable CA1069 // Enums values should not be duplicated
-        ACCENT_FLAG_ENABLE_GRADIENT_COLOR = 1 << 1,
-#pragma warning restore CA1069 // Enums values should not be duplicated
-
-        // As stated in the xml of the previous member,
-        // ACCENT_FLAG_ENABLE_MODERN_ACRYLIC_RECIPE and ACCENT_FLAG_ENABLE_GRADIENT_COLOR share
-        // the same value, which is why we supress CA1069
+        ACCENT_FLAG_ENABLE_MODERN_ACRYLIC_RECIPE = ACCENT_FLAG_ENABLE_GRADIENT_COLOR,
 
         ACCENT_FLAG_ENABLE_FULLSCREEN = 1 << 2,
         ACCENT_FLAG_ENABLE_BORDER_LEFT = 1 << 5,
