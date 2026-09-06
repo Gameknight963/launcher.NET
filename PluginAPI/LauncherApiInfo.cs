36b50ac6f9130e02a32cb4d97a42aaf82ea28fb6
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-09-05 20:46:12 -0500

    fix folder browsers dialogs in game from url by running them on sta threads

--- PluginAPI/LauncherApiInfo.cs
+++ PluginAPI/LauncherApiInfo.cs
@@ -4,7 +4,7 @@ namespace launcherdotnet.PluginAPI
 {
     public class LauncherApiInfo
     {
-        public static readonly SemVersion ApiVersion = new(2, 2, 0);
+        public static readonly SemVersion ApiVersion = new(2, 3, 0);
         public static string ApiVersionString => ApiVersion.ToString();
     }
 }
